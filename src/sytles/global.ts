2cea67d0ee3a19e2ba31b1024ac3944742f671ea
Diogo Novaes <diogonovaes@gmail.com>
2021-05-10 21:40:48 +0200

    chapter 2 finished

--- src/sytles/global.ts
+++ src/sytles/global.ts
@@ -2,7 +2,7 @@ import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
   :root {
-    --background: black;
+    --background: #f0f2f5;
     --red: #E52E4D;
     --blue: #5429CC;
     --green: #33CC95;
@@ -54,5 +54,40 @@ export const GlobalStyle = createGlobalStyle`
     cursor: not-allowed;
   }
 
+  .react-modal-overlay {
+    background: rgba(0, 0, 0, 0.5);
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    left: 0;
+
+    display: flex;
+    align-items: center;
+    justify-content: center
+  }
+
+  .react-modal-content {
+    width: 100%;
+    max-width: 576px;
+    background: var(--background);
+    padding: 3rem;
+    position: relative;
+    border-radius: 0.24rem;
+  }
+
+  .react-modal-close {
+    position: absolute;
+    right: 1.5rem;
+    top: 1.5rem;
+    border: 0;
+    background: transparent;
+    
+    &:hover {
+    filter: brightness(0.8)
+  }
+  }
+
+  
 
 `;
